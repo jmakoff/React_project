41bc9b6f5afba8884f17299cc11731e724e33945
juramak <juramak98@gmail.com>
2016-09-20 19:27:37 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/// <binding ProjectOpened='Watch - Development' />
+"use strict";
+
+module.exports = {
+    entry: {
+        "src/build/bundle": "./src/main.jsx",
+
+    } ,
+
+    output: {
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
