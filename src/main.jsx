e9b27124e8d2e82a20b0a49d29b1046a9520034f
juramak <juramak98@gmail.com>
2016-09-22 13:40:21 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,32 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 var Slider = require('react-slick');
-var ReactSlick = React.createClass({
+
+var ReactSlick = React.createClass({ //slider
     render: function () {
         var settings = {
-            docs: true
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+
         }
         return (
             <div className="slider">
                 <Slider {...settings}>
-                    <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"/>
-                    <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"/>
-                    <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"/>
-                    <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"/>
-                    <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"/>
+                    <img src="http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg"/>
+                    <img src="http://www.planwallpaper.com/static/images/wallpapers-20.jpg"/>
+                    <img src="http://www.planwallpaper.com/static/images/ZhGEqAP.jpg"/>
                     {/*<div className="text-center" id="text_1"><h3>THE HAM IS A PSD TEMPLATE</h3></div>
-                    <div className="text-center" id="text_2">WE ARE CREATIVE</div>
-                    <div className="text-center" id="text_3"><h4>quis nostrud exercitation ullamco laboris nisi </h4></div>
-                    <div className="my_icon my_icon_next"></div>
-                    <div className="my_icon my_icon_prev"></div>
+                     <div className="text-center" id="text_2">WE ARE CREATIVE</div>
+                     <div className="text-center" id="text_3"><h4>quis nostrud exercitation ullamco laboris nisi </h4></div>
+                     <div className="my_icon my_icon_next"></div>
+                     <div className="my_icon my_icon_prev"></div>
 
-                    <div id="buttons">
-                        <button className="btn my-btn" >EXPLORE NOW</button>
-                        <button className="btn my-btn" id="sec-btn">PURCHASE NOW</button>
-                    </div>*/}
-                </Slider>
+                     <div id="buttons">
+                     <button className="btn my-btn" >EXPLORE NOW</button>
+                     <button className="btn my-btn" id="sec-btn">PURCHASE NOW</button>
+                     </div>*/}
+                </Slider>{/*here carousel*/}
 
             </div>
         )
@@ -33,4 +35,4 @@ var ReactSlick = React.createClass({
 })
 
 
-ReactDom.render(<ReactSlick/>, document.getElementById('first-block'));
+ReactDom.render(<ReactSlick/>, document.getElementById('first-block'));/*carousel*/
